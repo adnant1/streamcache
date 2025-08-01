f03375d6a6868b54131e0b26392a703cd0486064
adnant1 <adnantayib8@gmail.com>
2025-08-01 00:03:26 -0400

    feat: Implemented initial REPL loop

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,31 @@
 #include <iostream>
 #include <string>
+#include <sstream>
+#include <vector>
 
+/*
+ * Core runtime REPL loop for the engine.
+ */
 int main() {
+
+    while(true) {
+        std::cout << "> " << std::flush;
+
+        std::string input;
+        std::getline(std::cin, input);
+
+        std::istringstream iss(input);
+        std::string token;
+        std::vector<std::string> tokens;
+
+        while (iss >> token) {
+            tokens.push_back(token);
+        }
+
+        if (input == "EXIT") {
+            break;
+        }
+    }
 
     return 0;
 }
